e7014d52a4fb7b4ee1767a0e75016e5b7b55fbbf
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2014-07-05 00:11:31 +0200

    Update ngcp-hep-logger.sh

--- ngcp-hep-logger.sh
+++ ngcp-hep-logger.sh
@@ -1,40 +1,88 @@
 #!/bin/bash
 # Simple experimental script to send HEP Encapsulated NGCP Logs to HEP Server/Collector
 # For more information: http://hep.sipcapture.org / hep@sipcapture.org
-# Version 0.1 - ALPHA ONLY
+# Version 0.2 - ALPHA ONLY
 
 
 # HEP Server 
 hepserver=127.0.0.1
-heport=9069
+heport=9063
 hepid=199
 heptype=100
 localip=$(/sbin/ip -4 -o addr show dev eth0| awk '{split($4,a,"/");print a[1]}')
+syslogport=5514
 
-# Specify NGCP Log file to monitor (ie: /var/log/ngcp/kamailio-lb.log) or all logs NGCP logs
-if ! [ $1 ]; then
-	echo "Missing Argument! USAGE EXAMPLE:"; echo;
-	echo "SINGLE LOG:    $0 /path/to/application.log";
-	echo "ALL LOGS  :    $0 --all";
-	exit;
+# Bypass filter for CALL-ID (match=exclude)
+skip="*127.0.0.1"
+# Bypas filter for full log line (match=exclude)
+skipline="*udp:127.0.0.1:5060*"
+
+# script execution vars
+script="tail -f"
+netcat="nc -k -l ${syslogport}"
+command=$script
+
+# Usage
+usage() {
+	echo "Usage: $0 [-l </path/to/file.log>] [-a </path/to/logs>] [-x <string>]"
+	echo
+	echo "  -l <file>	:	Read specified Log file"
+	echo "  -a <dir>	:	Read *.log in specified directory"
+	echo "  -n <port>	:	Read lines from TCP socket"
+	echo "  -x <*string>	:	Exclude all IDs matching Regex"
+	1>&2; exit 1; 
+}
+
+while getopts ":n:l:a:xqh" o; do
+    case "${o}" in
+        n)
+            n=${OPTARG}
+            syslogport=${OPTARG}
+            command="${netcat}"
+	    log=""
+		echo "--NETWORK MODE SELECTED, PORT ${syslogport}"
+            ;;
+        l)
+            l=${OPTARG}
+	    log=${OPTARG}
+		echo "--LOG MODE SELECTED, READING FROM ${OPTARG}"
+            ;;
+        a)
+            a=${OPTARG}
+	    log=${OPTARG}/*.log
+	    all=1
+		echo "--ALL MODE SELECTED, READING ALL FROM DIRECTORY ${OPTARG}"
+            ;;
+        x)
+            x=${OPTARG}
+	    skip=${OPTARG}
+		echo "--EXCLUDING IDs MATCHING: ${OPTARG}"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [ -z "${l}" ] && [ -z "${a}" ] && [ -z "${n}" ] ; then
+    usage
 fi
 
-if [ $1 = "--all" ] || [ $1 = "-a" ]; then
-	log=/var/log/ngcp/*.log
-	all=1
-else 
-	log=$1
-fi
 
+# stats counters
+sent=0
+fail=0
 
-#test for hepipe
+# test for hepipe
 if ! [ $(which hepipe) ]; then
 	echo "ERROR: Please install HEPipe (https://github.com/sipcapture/hepipe)";
 	exit;
 fi
 
-#test for existence of the log file
-if [ $all = 1 ] || [ -f $log ]; then
+# If needed, check for existance of logs
+if [[ $all == 1 ]] || [[ -f $log ]]; then
 
   #read through the file looking for the Call ID=
   while read line
@@ -42,13 +90,32 @@ if [ $all = 1 ] || [ -f $log ]; then
     echo $line | grep -q ID=
     if [ $? == 0 ]; then
     callid=`echo $line  | grep --line-buffered "ID=" | sed -u -e "s/.*ID=//" | sed -u -e "s/\"//g"`
-    logdate=`echo $line | awk '{print $1, $2, $3}'`
-    ts=$(date --date="$logdate" +%s)
-    tsu=$(date --date="$logdate" +%4N)
-    #### timesec;timeusec;correlationid;source_ip;source_port;destination_ip;destinaton_port;payload in json
-    #echo "${ts};${tsu};${callid};127.0.0.1;5060;10.0.0.1;5060;{\"log\": \"${line}\"}"
-    echo "${ts};${tsu};${callid};${localip};514;${localip};514;{\"log\": \"${line}\"}" | hepipe  -s $hepserver -p $heport -i $hepid -t $heptype 
+	if [[ $callid == $skip ]]; then
+		#echo " Skipping Call-ID..."
+		fail=$[fail + 1];
+	elif [[ $line == $skipline ]]; then
+		#echo " Skipping Line..."
+		fail=$[fail + 1];
+	else
+	    sent=$[sent + 1];
+	    line="${line//$'\r'/$'\n'}" 
+	    	#message=`echo $line | awk '{ s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }'`
+	    	#proc=`echo $line | awk '{print $4, $5}'`
+	    logdate=`echo $line | awk '{print $1, $2, $3}'`
+	    ts=$(date --date="$logdate" +%s)
+	    tsu=$(date --date="$logdate" +%4N)
+	    
+	    #### HEPipe HEADER PARAMS: #############################################################################################
+	    #### timesec;timeusec;correlationid;source_ip;source_port;destination_ip;destinaton_port;payload in json OR string  ####
+	    ########################################################################################################################
+
+	    ## JSON Version
+	    #echo "${ts};${tsu};${callid};${localip};514;${localip};514;{\"log\": \"${message}\"}" | hepipe  -s $hepserver -p $heport -i $hepid -t $heptype
+
+	    ## STRING Version
+	    echo "${ts};${tsu};${callid};${localip};${syslogport};${localip};${syslogport};${line}" | hepipe  -s $hepserver -p $heport -i $hepid -t $heptype
+	fi
     fi
-  done < <(tail -f $log)
+  done < <($command $log)
 
 fi
